eab5e29d5897adf4fe12f9ce55917744a51beaac
Diego López <dieguitarra@gmail.com>
2022-03-09 14:36:00 -0300

    se cambiaron cosas

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./component/NavBar/NavBar";
 import ItemListContainer from "./Container/ItemListContainer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell, faComment } from "@fortawesome/free-solid-svg-icons";
-import { faFacebook, faApple } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
