f1c9130e6595ed759e7395780ee3ee31afeb178a
Diego López <dieguitarra@gmail.com>
2022-03-26 19:01:08 -0300

    desafío clase 9

--- src/component/ItemDetail/ItemDetail.jsx
+++ src/component/ItemDetail/ItemDetail.jsx
@@ -1,23 +1,47 @@
 import { useCartContext } from "../../context/CartContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Input } from "../../Input";
 import ItemCount from "../ItemCount/ItemCount";
-import Intercambiabilidad from "../Intercambiabilidad";
+import { useNavigate } from "react-router-dom";
 function ItemDetail({ producto }) {
+  const [comprar, setComprar] = useState(false);
+  const [count, setCount] = useState(1);
   const { addToCart } = useCartContext();
+  const navegar = useNavigate();
   const onAdd = (cant) => {
     console.log(cant);
     addToCart({ ...producto, cantidad: cant });
+    setComprar(true);
   };
   // console.log(cartList);
   return (
     <div>
-      <Intercambiabilidad />
       <img src={producto.foto} alt="imagen" />
       <p className="alert alert-primary w-25 mx-auto">{producto.name}</p>
       <p className="alert alert-primary w-25 mx-auto">{producto.price}</p>
       <p className="alert alert-primary w-25 mx-auto">{producto.categoria}</p>
-      <ItemCount initial={1} stock={5} onAdd={onAdd} />
+      {!comprar && (
+        <ItemCount
+          initial={1}
+          stock={5}
+          onAdd={onAdd}
+          count={count}
+          setCount={setCount}
+        />
+      )}
+      {comprar && (
+        <div>
+          <button className="btn btn-primary mx-2" onClick={() => navegar("/")}>
+            Seguir comprando
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => navegar("/cart")}
+          >
+            Ir al carrito
+          </button>
+        </div>
+      )}
     </div>
   );
 }
